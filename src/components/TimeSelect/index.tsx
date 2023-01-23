5af9cc426f7b2017efd80ab1f03bdc9a0eca50b2
Gabriel Mateus <gabriel.dorneles@prestadores.finnet.com.br>
2023-01-23 13:22:52 -0300

    Creating a select time component and update store context

--- src/components/TimeSelect/index.tsx
+++ src/components/TimeSelect/index.tsx
@@ -1,7 +1,22 @@
+import { useContext } from 'react';
+
+import { AppContext } from '../../store/context';
+import { SelectTime } from './styles';
+
 const TimeSelect = () => {
+  const { setSelectedTime } = useContext(AppContext);
+  const handleChangeOption = (optionSelected: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedTime(optionSelected.target.value);
+  };
   return (
     <>
-      <h1>time select</h1>
+      <p>selecione um horário</p>
+      <SelectTime onChange={handleChangeOption}>
+        <option>00:30</option>
+        <option>01:00</option>
+        <option>01:30</option>
+        <option>02:00</option>
+      </SelectTime>
     </>
   );
 };
--- src/store/context/index.tsx
+++ src/store/context/index.tsx
@@ -3,14 +3,20 @@ import { createContext, Dispatch, SetStateAction, useState } from 'react';
 interface StatesContextType {
   listTasks: any[];
   setListTasks: Dispatch<SetStateAction<any>>;
+  selectedTime: string;
+  setSelectedTime: Dispatch<SetStateAction<string>>;
 }
 
 export const AppContext = createContext({} as StatesContextType);
 
 export const ContextProvider = ({ children }: any) => {
   const [listTasks, setListTasks] = useState([]);
+
+  const [selectedTime, setSelectedTime] = useState('');
   return (
-    <AppContext.Provider value={{ listTasks, setListTasks }}>
+    <AppContext.Provider
+      value={{ listTasks, setListTasks, selectedTime, setSelectedTime }}
+    >
       {children}
     </AppContext.Provider>
   );
